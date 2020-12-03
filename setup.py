fc9cab344d37aafd6bb8fffb0ee63f3a4978996b
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-03 23:51:37 +0300

    2020:12:03 23:51

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='docker-upload-and-build',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
     scripts=['scripts/docker-upload-and-build']
